fca7a103b1f8ffff8db2dabd6026bc3aaf98fa7c
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-11-10 14:33:42 -0500

    manualy removed duplicate column names in time_dependent_cardiovascular.txt

--- proc_NURSA_expression_data.py
+++ proc_NURSA_expression_data.py
@@ -16,25 +16,38 @@ def main():
   ###################
   all_exp = []
   all_gene = []
+  all_id = []
   for i in range(len(lines)):
+
     inst_line = lines[i].strip().split('\t')
 
-    all_exp.append(inst_line[1])
-
-  print(len(all_exp))
-  all_exp = sorted(list(set(all_exp)))
-  print(len(all_exp))
-  print(all_exp)
-
-  # gather data
-  ###################
-  for i in range(len(lines)):
-    inst_line = lines[i].strip().split('\t')
     if i > 0:
 
-      inst_col = inst_line[1]
-      inst_row = inst_line[2]
-      inst_val = inst_line[4]
+      all_id.append(inst_line[0])
+      all_exp.append(inst_line[1])
+
+  all_id = list(set(all_id))
+  print(all_id)
+
+  # print('\n\n')
+  # print(len(all_exp))
+  # all_exp = sorted(list(set(all_exp)))
+  # print(len(all_exp))
+  # print(all_exp)
+
+  # all_id = list(set(all_id))
+  # print(len(all_id))
+  # print(all_id)
+
+  # # gather data
+  # ###################
+  # for i in range(len(lines)):
+  #   inst_line = lines[i].strip().split('\t')
+  #   if i > 0:
+
+  #     inst_col = inst_line[1]
+  #     inst_row = inst_line[2]
+  #     inst_val = inst_line[4]
 
 
   # save matrix
