f83aad358f9ee74ff5a01d6a13b3f2b13053e932
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-11-10 13:38:51 -0500

    working on script that converts NURSA expression data format to matrix

--- proc_NURSA_expression_data.py
+++ proc_NURSA_expression_data.py
@@ -5,9 +5,29 @@ def main():
   lines = f.readlines()
   f.close()
 
+  # initialize matrix
+  ###################
+
+
+  # gather data
+  ###################
   for i in range(len(lines)):
-    inst_line = lines[i].strip().split()
+    inst_line = lines[i].strip().split('\t')
     if i > 0:
-      print(inst_line)
+      # 0: Experiment ID
+      # 1: Experiment Name
+      # 2: Symbol
+      # 3: Entrez Gene ID
+      # 4: Fold Change
+      # 5: p-value
+
+      inst_col = inst_line[1]
+      inst_row = inst_line[2]
+      inst_val = inst_line[4]
+
+
+  # save matrix
+  ###################
+
 
 main()
