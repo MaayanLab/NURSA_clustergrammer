26bc964479f1148e14ee05346d837b8bcb7053d4
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-11-10 14:21:55 -0500

    swapped in micro for greek symbol

--- proc_NURSA_expression_data.py
+++ proc_NURSA_expression_data.py
@@ -5,21 +5,32 @@ def main():
   lines = f.readlines()
   f.close()
 
+  # 0: Experiment ID
+  # 1: Experiment Name
+  # 2: Symbol
+  # 3: Entrez Gene ID
+  # 4: Fold Change
+  # 5: p-value
+
   # initialize matrix
   ###################
+  all_exp = []
+  all_gene = []
+  for i in range(len(lines)):
+    inst_line = lines[i].strip().split('\t')
 
+    all_exp.append(inst_line[1])
+
+  print(len(all_exp))
+  all_exp = sorted(list(set(all_exp)))
+  print(len(all_exp))
+  print(all_exp)
 
   # gather data
   ###################
   for i in range(len(lines)):
     inst_line = lines[i].strip().split('\t')
     if i > 0:
-      # 0: Experiment ID
-      # 1: Experiment Name
-      # 2: Symbol
-      # 3: Entrez Gene ID
-      # 4: Fold Change
-      # 5: p-value
 
       inst_col = inst_line[1]
       inst_row = inst_line[2]
