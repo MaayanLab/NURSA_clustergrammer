137740f388c88a493c24dae6fb028812c72a46f3
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-11-10 17:50:34 -0500

    working on making matrix, will break into two separate matrices

--- proc_NURSA_expression_data.py
+++ proc_NURSA_expression_data.py
@@ -1,4 +1,6 @@
 def main():
+  import numpy as np
+  import pandas as pd
 
   filename = 'data/time_dependent_cardiovascular.txt'
   f = open(filename, 'r')
@@ -16,45 +18,39 @@ def main():
   ###################
   all_exp = []
   all_gene = []
-  all_id = []
+
   for i in range(len(lines)):
 
     inst_line = lines[i].strip().split('\t')
 
     if i > 0:
 
-      all_id.append(inst_line[0])
       all_exp.append(inst_line[1])
+      all_gene.append(inst_line[2])
 
-  all_id = sorted(list(set(all_id)))
-  print(all_id)
-
-  print('\n\n')
-  print(len(all_exp))
   all_exp = sorted(list(set(all_exp)))
-  print(len(all_exp))
+  all_gene = sorted(list(set(all_gene)))
 
-  print('-- experiments')
-  for inst_exp in all_exp:
-    print(inst_exp)
+  mat = np.zeros([len(all_gene), len(all_exp)])
 
-  # all_id = list(set(all_id))
-  # print(len(all_id))
-  # print(all_id)
+  # gather data
+  ###################
+  for i in range(len(lines)):
+    inst_line = lines[i].strip().split('\t')
+    if i > 0:
 
-  # # gather data
-  # ###################
-  # for i in range(len(lines)):
-  #   inst_line = lines[i].strip().split('\t')
-  #   if i > 0:
+      inst_exp = inst_line[1]
+      inst_gene = inst_line[2]
+      inst_val = np.log2(float(inst_line[4]))
 
-  #     inst_col = inst_line[1]
-  #     inst_row = inst_line[2]
-  #     inst_val = inst_line[4]
+      row_index = all_gene.index(inst_gene)
+      col_index = all_exp.index(inst_exp)
 
+      mat[row_index, col_index] = inst_val
 
   # save matrix
   ###################
-
+  df = pd.DataFrame(data=mat, columns=all_exp, index=all_gene)
+  df.to_csv('data/td_cardio_matrix.txt', sep='\t')
 
 main()
